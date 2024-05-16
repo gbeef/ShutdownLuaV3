2641888b0f4e668e8dfc5a16625bc2ebc7bd5cd3
Matthew <119807878+ParaMattKoopa@users.noreply.github.com>
2024-05-16 07:32:44 -0400

    Update Distractions.bat
    Closes more things
    including sonic dawt e ex e

--- extern/Distractions.bat
+++ extern/Distractions.bat
@@ -9,13 +9,22 @@ taskkill /F /IM "opera.exe" /T
 
 :: Game Launchers
 taskkill /F /IM "steam.exe" /T
-taskkill /F /IM "EpicGamesLauncher.exe" /T
+taskkill /F /IM "epicgameslauncher.exe" /T
 
 :: Dumb Shtuff
 taskkill /F /IM "calc.exe" /T
 taskkill /F /IM "notepad.exe" /T
+taskkill /F /IM "paint.exe" /T
+taskkill /F /IM "sonic.exe" /T
 
 :: MS Office
 taskkill /F /IM "winword.exe" /T
 taskkill /F /IM "excel.exe" /T
 taskkill /F /IM "powerpnt.exe" /T
+
+:: More Stuff
+taskkill /F /IM "spotify.exe" /T
+
+:: Adobe
+taskkill /F /IM "photoshop.exe" /T
+taskkill /F /IM "adobe premiere pro.exe" /T
